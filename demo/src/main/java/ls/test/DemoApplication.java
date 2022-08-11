50733325fc21ddcdf986cc8198967d8d67a48667
Umer <Umer@DESKTOP-PPOR226>
2022-08-11 15:22:17 +0530

    intial commmit

---
+++ demo/src/main/java/ls/test/DemoApplication.java
@@ -0,0 +1,22 @@
+package ls.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	@GetMapping("/hello")
+	public String demo() {
+		return "hello World:";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
